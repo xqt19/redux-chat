767c076a6f23b2516f0c119e27af9793315039a7
xqt19 <tan.xiu.qi.11@gmail.com>
2020-10-12 19:46:57 +0800

    updated redux-logger

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {logger}  from 'redux-logger'
 
 // internal modules
 import App from './components/app';
@@ -13,9 +14,12 @@ const reducers = combineReducers({
   changeMe: (state = null, action) => state
 });
 
+// Middleware
+const middlewares = applyMiddleware(logger);
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
