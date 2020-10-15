a71e6131ed9cf06b87cfbb313619b75602902ea1
xqt19 <tan.xiu.qi.11@gmail.com>
2020-10-15 14:14:03 +0800

    created skeleton for chatroom app

--- src/containers/testReducer/text_reducer.js
+++ src/containers/testReducer/text_reducer.js
@@ -1,6 +1,9 @@
-const textReducer = (state=null, action) =>{
+import {TEXT_YO, TEXT_CLEAR, NEW_JOKE} from './textYo'
+
+
+export const textReducer = (state=null, action) =>{
     switch (action.type){
-        case 'TEXT_YO':
+        case TEXT_YO:
             if (action.payload != "") {
                 if (state == null){
                     return [action.payload]
@@ -8,10 +11,18 @@ const textReducer = (state=null, action) =>{
                 return [...state, action.payload]
             }
             return state
-        case 'TEXT_CLEAR':
+        case TEXT_CLEAR:
             return null
         default:
             return state
     }
 }
-export default textReducer
+
+export const jokeReducer = (state=null, action) =>{
+    switch(action.type){
+        case NEW_JOKE:
+            return action.payload
+        default:
+            return state
+    }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import {logger}  from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import {logger}  from 'redux-logger';
+import reduxPromise from 'redux-promise'
 
 // internal modules
 import App from './components/app';
@@ -11,14 +12,15 @@ import '../assets/stylesheets/application.scss';
 
 
 // State and reducers
-import textReducer from './containers/testReducer/text_reducer';
+import {textReducer, jokeReducer} from './containers/testReducer/text_reducer';
 const reducers = combineReducers({
-  changeMe: textReducer, //dummy testreducer
-
+  changeMe: textReducer, //dummy text reducer
+  changeMe2: jokeReducer, //dummy chuck norris joke promise reducer
 });
 
 // Middleware
-const middlewares = applyMiddleware(logger);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger,reduxPromise));
 
 // render an instance of the component in the DOM
 ReactDOM.render(
